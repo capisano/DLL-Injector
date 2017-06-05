000f3fb350c7bc2e3905e3e40fbd5b38789f3d70
dave <capisano@goat.si>
2017-06-05 17:17:57 -0500

    Added source and sln files

---
+++ Private Injector/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.ListBox1 = New System.Windows.Forms.ListBox()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'ProgressBar1
+		'
+		Me.ProgressBar1.BackColor = System.Drawing.SystemColors.ControlDark
+		Me.ProgressBar1.Location = New System.Drawing.Point(194, 590)
+		Me.ProgressBar1.Name = "ProgressBar1"
+		Me.ProgressBar1.Size = New System.Drawing.Size(350, 27)
+		Me.ProgressBar1.TabIndex = 1
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.Color.Transparent
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.White
+		Me.Label1.Location = New System.Drawing.Point(6, 16)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(89, 20)
+		Me.Label1.TabIndex = 2
+		Me.Label1.Text = "Proc name:"
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.Color.Transparent
+		Me.Button1.Location = New System.Drawing.Point(20, 590)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(168, 27)
+		Me.Button1.TabIndex = 3
+		Me.Button1.Text = "Inject"
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+		Me.GroupBox1.Controls.Add(Me.Label8)
+		Me.GroupBox1.Controls.Add(Me.Label7)
+		Me.GroupBox1.Controls.Add(Me.Label5)
+		Me.GroupBox1.Controls.Add(Me.ComboBox1)
+		Me.GroupBox1.Controls.Add(Me.Label4)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(178, 190)
+		Me.GroupBox1.TabIndex = 5
+		Me.GroupBox1.TabStop = False
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.BackColor = System.Drawing.Color.Transparent
+		Me.Label8.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label8.Location = New System.Drawing.Point(26, 56)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(21, 20)
+		Me.Label8.TabIndex = 8
+		Me.Label8.Text = "..."
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(68, 160)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(16, 13)
+		Me.Label7.TabIndex = 7
+		Me.Label7.Text = "..."
+		Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.BackColor = System.Drawing.Color.Transparent
+		Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label5.ForeColor = System.Drawing.Color.White
+		Me.Label5.Location = New System.Drawing.Point(4, 81)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(107, 20)
+		Me.Label5.TabIndex = 5
+		Me.Label5.Text = "Injection Type"
+		'
+		'ComboBox1
+		'
+		Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+		Me.ComboBox1.FormattingEnabled = True
+		Me.ComboBox1.Items.AddRange(New Object() {"Manual Mapping", "Load Library", "Thread Hijack"})
+		Me.ComboBox1.Location = New System.Drawing.Point(10, 104)
+		Me.ComboBox1.Name = "ComboBox1"
+		Me.ComboBox1.Size = New System.Drawing.Size(149, 21)
+		Me.ComboBox1.TabIndex = 4
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.BackColor = System.Drawing.Color.Transparent
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.ForeColor = System.Drawing.Color.White
+		Me.Label4.Location = New System.Drawing.Point(6, 36)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(104, 20)
+		Me.Label4.TabIndex = 3
+		Me.Label4.Text = "Library name:"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.BackColor = System.Drawing.Color.Transparent
+		Me.Label2.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.ForeColor = System.Drawing.Color.Red
+		Me.Label2.Location = New System.Drawing.Point(535, -1)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(21, 20)
+		Me.Label2.TabIndex = 6
+		Me.Label2.Text = "X"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.BackColor = System.Drawing.Color.Transparent
+		Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label3.ForeColor = System.Drawing.SystemColors.WindowFrame
+		Me.Label3.Location = New System.Drawing.Point(510, -1)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(19, 20)
+		Me.Label3.TabIndex = 7
+		Me.Label3.Text = "_"
+		'
+		'ListBox1
+		'
+		Me.ListBox1.BackColor = System.Drawing.SystemColors.Window
+		Me.ListBox1.FormattingEnabled = True
+		Me.ListBox1.Location = New System.Drawing.Point(13, 241)
+		Me.ListBox1.Name = "ListBox1"
+		Me.ListBox1.Size = New System.Drawing.Size(178, 342)
+		Me.ListBox1.Sorted = True
+		Me.ListBox1.TabIndex = 8
+		'
+		'Button2
+		'
+		Me.Button2.Location = New System.Drawing.Point(13, 208)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(177, 27)
+		Me.Button2.TabIndex = 9
+		Me.Button2.Text = "Re-List"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.Control
+		Me.BackgroundImage = Global.Private_Injector.My.Resources.Resources.capisano
+		Me.ClientSize = New System.Drawing.Size(556, 628)
+		Me.ControlBox = False
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.ListBox1)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.ProgressBar1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "Form1"
+		Me.Text = "Plastic Injector"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Private Injector/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports InjectionLibrary
+Imports JLibrary.PortableExecutable
+Imports System.IO
+Public Class Form1
+    Public Filepath As String
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+        For Each OneProcess As Process In Process.GetProcesses
+            ListBox1.Items.Add(OneProcess.ProcessName)
+        Next
+    End Sub
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+    Public Function MMInject(ByVal pID As Integer, ByVal DLL As Byte()) As Boolean
+        Try
+            Dim injector As InjectionMethod = InjectionMethod.Create(InjectionMethodType.ManualMap)
+            Dim hModule As IntPtr = IntPtr.Zero
+
+            Using img As New PortableExecutable(DLL)
+                hModule = injector.Inject(img, pID)
+            End Using
+            If hModule <> IntPtr.Zero Then
+                DLL = Nothing
+                Return True
+            Else
+                DLL = Nothing
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox("Injection Failed...", 47, "Error: 0x001")
+            End
+        End Try
+    End Function
+    Public Function THInject(ByVal pID As Integer, ByVal DLL As Byte()) As Boolean
+        Try
+            Dim injector As InjectionMethod = InjectionMethod.Create(InjectionMethodType.ThreadHijack)
+            Dim hModule As IntPtr = IntPtr.Zero
+            Using img As New PortableExecutable(DLL)
+                hModule = injector.Inject(img, pID)
+            End Using
+            If hModule <> IntPtr.Zero Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox("Injection Failed...", 47, "Error: 0x001")
+            End
+        End Try
+    End Function
+    Public Function LLInject(ByVal pID As Integer, ByVal DLL As Byte()) As Boolean
+        Try
+            Dim injector As InjectionMethod = InjectionMethod.Create(InjectionMethodType.Standard)
+            Dim hModule As IntPtr = IntPtr.Zero
+
+            Using img As New PortableExecutable(DLL)
+                hModule = injector.Inject(img, pID)
+            End Using
+            If hModule <> IntPtr.Zero Then
+                DLL = Nothing
+                Return True
+            Else
+                DLL = Nothing
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox("Injection Failed...", 47, "Error: 0x001")
+            End
+        End Try
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Filepath = Nothing Or ListBox1.SelectedItem = Nothing Then
+            Return
+        End If
+        Dim targeted As Process() = Process.GetProcessesByName(ListBox1.SelectedItem)
+        Dim pid As Integer = targeted(0).Id
+        Dim DLL As Byte() = File.ReadAllBytes(Filepath)
+        ProgressBar1.Value = 50
+        If ComboBox1.SelectedItem.ToString = "Manual Mapping" Then
+            If MMInject(pid, DLL) Then
+                ProgressBar1.Value = 100
+                Label7.ForeColor = Color.Green
+                Label7.Text = "Success"
+                Return
+            Else
+                ProgressBar1.Value = 0
+                Label7.ForeColor = Color.Red
+                ProgressBar1.BackColor = Color.Red
+                Label7.Text = "Failed"
+                Return
+            End If
+        ElseIf ComboBox1.SelectedItem.ToString = "Load Library" Then
+            If LLInject(pid, DLL) Then
+                ProgressBar1.Value = 100
+                Label7.ForeColor = Color.Green
+                Label7.Text = "Success"
+                Return
+            Else
+                ProgressBar1.Value = 0
+                Label7.ForeColor = Color.Red
+                ProgressBar1.BackColor = Color.Red
+                Label7.Text = "Failed"
+                Return
+            End If
+        ElseIf ComboBox1.SelectedItem.ToString = "Thread Hijack" Then
+            If THInject(pid, DLL) Then
+                ProgressBar1.Value = 100
+                Label7.ForeColor = Color.Green
+                Label7.Text = "Success"
+                Return
+            Else
+                ProgressBar1.Value = 0
+                Label7.ForeColor = Color.Red
+                ProgressBar1.BackColor = Color.Red
+                Label7.Text = "Failed"
+                Return
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Dim dialog As New OpenFileDialog()
+        If DialogResult.OK = dialog.ShowDialog Then
+            Label4.Text = dialog.FileName
+        End If
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+        Dim dialog As New OpenFileDialog()
+        dialog.Filter = "Dynamic Link Library(.dll) |*.dll"
+        If DialogResult.OK = dialog.ShowDialog Then
+            Filepath = dialog.FileName
+            Dim dirpath As String = Path.GetFileName(Filepath)
+            Label8.Text = dirpath
+        End If
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        For Each OneProcess As Process In Process.GetProcesses
+            ListBox1.Items.Add(OneProcess.ProcessName)
+        Next
+    End Sub
+    Private IsFormBeingDragged As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = True
+            MouseDownX = e.X
+            MouseDownY = e.Y
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseUp
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = False
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseMove
+
+        If IsFormBeingDragged Then
+            Dim temp As Point = New Point()
+
+            temp.X = Me.Location.X + (e.X - MouseDownX)
+            temp.Y = Me.Location.Y + (e.Y - MouseDownY)
+            Me.Location = temp
+            temp = Nothing
+        End If
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Label1.Text = "Proccess: " & ListBox1.SelectedItem
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
